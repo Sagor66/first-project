af020f95f21625234d48d15ed8b4b202b6de0548
sagor66 <sazzadulhaquesagor@gmail.com>
2024-12-20 15:21:18 +0600

    more recator

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRouts } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 // const port = 3000;
 
@@ -12,8 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRouts);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 // test if server is running
 app.get('/', (req: Request, res: Response) => {
