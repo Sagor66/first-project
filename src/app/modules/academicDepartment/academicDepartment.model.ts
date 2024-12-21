5272d9fc3130c3a28ec253e84d40d15df5cd1ca2
sagor66 <sazzadulhaquesagor@gmail.com>
2024-12-21 20:54:41 +0600

    added: pre middleware for query find one and update in academic department

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -16,6 +16,30 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   { timestamps: true },
 );
 
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExists = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExists) {
+    throw new Error('This department is already exists');
+  }
+
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+
+  const isDepartmentExists = await AcademicDepartment.findOne({ query });
+
+  if (!isDepartmentExists) {
+    throw new Error('This department does not already exists');
+  }
+
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
