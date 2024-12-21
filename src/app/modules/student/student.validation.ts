1117b73d5b015bc11de4d73e71ab4584e69b5715
sagor66 <sazzadulhaquesagor@gmail.com>
2024-12-21 21:09:54 +0600

    added: populate which works like preload

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -82,6 +82,7 @@ const createStudentValidationSchema = z.object({
       localGuardian: localGuardianValidationSchema,
       profileImg: z.string().optional(),
       admissionSemester: z.string(),
+      academicDepartment: z.string(),
     }),
   }),
 });
